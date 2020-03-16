481127d5cf51848029b2d8d3cff97b0d72d47e5e
Taylor Lohman <taylorlohman@Taylors-MacBook-Pro.local>
2020-03-15 23:08:01 -0700

    play

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import { render } from 'react-dom';
+import React, { useState } from 'react';
+import { useSpring, animated } from 'react-spring';
+import { useGesture } from 'react-use-gesture';
+import useMeasure from './useMeasure';
+import Triangle from "./triangle";
 import './App.css';
 
 function App() {
+  const [open, toggle] = useState(false)
+  const [bind, { height }] = useMeasure()
+  const props = useSpring({ height: open ? 0 : height })
+  
+
+  const vertices = [
+    [0, 60],
+    [30, 0],
+    [60, 60],
+  ];
+
+  // <svg width="400" height="400">
+  //       <Triangle vertices={ vertices }
+  //           color="red"
+  //           delay={ 0 } 
+  //  />
+  // </svg>
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div {...bind} class="main" onClick={() => toggle(!open)}>
+        <animated.div class="fill" style={props} />
+        <animated.div class="content">{props.height.interpolate(x => x.toFixed(0))}</animated.div>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
