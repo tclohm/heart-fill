dcee417ec036f95fc56a41a1278368f5c4f03387
Taylor Lohman <taylorlohman@Taylors-MacBook-Pro.local>
2020-03-16 08:31:40 -0700

    renamed pop to heart

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Heart from './pop';
+import Heart from './Heart';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<Heart color="#EB2B2B" time={300} />, document.getElementById('root'));
