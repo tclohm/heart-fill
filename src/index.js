079b64502e41dfce67f37f8228e0db01a4748f42
Taylor Lohman <taylorlohman@Taylors-MacBook-Pro.local>
2020-03-16 08:28:08 -0700

    heart can be customized by color and duration

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Demo from './pop';
+import Heart from './pop';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Demo />, document.getElementById('root'));
+ReactDOM.render(<Heart color="#EB2B2B" time={300} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
